9c6ae870a3c1d019b4c92087761e19d126b88bc4
Victor Galan <victor.galan@liferay.com>
2018-10-24 00:15:25 +0200

    Add led

---
+++ led.js
@@ -0,0 +1,16 @@
+const Gpio = require('onoff').Gpio;
+const led = new Gpio(4, 'out');
+const blinkInterval = setInterval(blinkled, 250);
+
+function blinkled() {
+  if (led.readSync() === 0) {
+    led.writeSync(1);
+  } else {
+    led.writeSync(0);
+  }
+}
+
+process.on('SIGINT', () => {
+  clearInterval(blinkInterval);
+	led.unexport();
+});
