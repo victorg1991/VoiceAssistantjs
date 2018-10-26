5c596b1c20c842a8d0decedf874f922f90b0852f
Javier Gamarra <nhpatt@gmail.com>
2018-10-26 11:25:45 +0200

    exercise

--- dialogflow.js
+++ dialogflow.js
@@ -2,101 +2,87 @@
 
 const util = require('util');
 const fs = require('fs');
-const mic = require('mic');
+const dialogflow = require('dialogflow');
+// const mic = require('mic');
 
-const projectId = 'multilocalesample-a62ad';
-const sessionId = 'sessionId';
-const languageCode = 'es-ES';
-const rate = 16000;
 let filename = 'test.wav';
 // filename = 'audios/enciende_la_luz.wav';
-// filename = 'audios/apaga_la_luz_en_10_segundos.wav';
+filename = 'audios/apaga_la_luz_en_10_segundos.wav';
 // filename = 'audios/enciende_el_servo.wav';
 
 const config = {
-    rate,
+    rate: 16000,
     channels: 1,
     debug: true,
-    fileType: "wav",
+    fileType: 'wav',
     exitOnSilence: 30
 };
 
-const micInstance = mic(config);
-const stream = micInstance.getAudioStream();
+// const micInstance = mic(config);
+// const stream = micInstance.getAudioStream();
+//
+// const outputFileStream = fs.WriteStream(filename);
+//
+// stream.pipe(outputFileStream);
+//
+// micInstance.start();
 
-const outputFileStream = fs.WriteStream(filename);
+// stream.on('data', function (data) {
+//     console.log('Recieved Input Stream: ' + data.length);
+// });
+//
+// stream.on('error', function (err) {
+//     console.log('Error in Input Stream: ' + err);
+// });
+//
+// stream.on('startComplete', function () {
+//     console.log('Received startComplete');
+// });
+//
+// stream.on('stopComplete', function () {
+//     console.log('Received stopComplete');
+// });
+//
+// stream.on('pauseComplete', function () {
+//     console.log('Received pauseComplete');
+// });
+//
+// stream.on('resumeComplete', function () {
+//     console.log('Received resumeComplete');
+// });
+//
+// stream.on('silence', function () {
+//     console.log('Received silence... let\'s process it');
+//
+//     micInstance.stop();
+//
+//     detectAudioIntent(filename);
+// });
+//
+// stream.on('processExitComplete', function () {
+//     console.log('Got SIGNAL processExitComplete');
+//
+//     micInstance.stop();
+// });
 
-stream.pipe(outputFileStream);
+const sessionClient = new dialogflow.SessionsClient();
+const session = sessionClient.sessionPath('multilocalesample-a62ad', 'sessionId');
 
-micInstance.start();
+detectAudioIntent(session, filename);
 
-stream.on('data', function(data) {
-    console.log("Recieved Input Stream: " + data.length);
-});
-
-stream.on('error', function(err) {
-    console.log("Error in Input Stream: " + err);
-});
-
-stream.on('startComplete', function() {
-    console.log("Received startComplete");
-});
-
-stream.on('stopComplete', function() {
-    console.log("Received stopComplete");
-});
-
-stream.on('pauseComplete', function() {
-    console.log("Received pauseComplete");
-});
-
-stream.on('resumeComplete', function() {
-    console.log("Received resumeComplete");
-});
-
-stream.on('silence', function() {
-    console.log("Received silence... let's process it");
-
-    micInstance.stop();
-
-    detectAudioIntent(projectId, sessionId, filename, 'AUDIO_ENCODING_LINEAR_16', rate, languageCode);
-});
-
-stream.on('processExitComplete', function() {
-    console.log("Got SIGNAL processExitComplete");
-
-    micInstance.stop();
-});
-
-// detectAudioIntent(projectId, sessionId, filename, 'AUDIO_ENCODING_LINEAR_16', rate, languageCode);
-
-function detectAudioIntent(projectId, sessionId, filename, audioEncoding, sampleRateHertz, languageCode) {
-
-    const dialogflow = require('dialogflow');
-
-    const sessionClient = new dialogflow.SessionsClient();
-
-    const session = sessionClient.sessionPath(projectId, sessionId);
-
+function detectAudioIntent(session, filename) {
     const readFile = util.promisify(fs.readFile);
-    readFile(filename)
-        .then(inputAudio => {
-            const request = {
-                session, inputAudio,
-                queryInput: {audioConfig: {audioEncoding, sampleRateHertz, languageCode}},
-            };
-            return sessionClient.detectIntent(request);
-        })
-        .then(responses => {
-            console.log('Detected intent:');
-            console.log(sessionClient, responses[0].queryResult);
-            return responses[0].queryResult;
-        })
-        .then(result => {
-            console.log(`Parameter: ${result['parameters']['fields']['number'] ? result['parameters']['fields']['number']['numberValue'] : ''}`);
-            console.log(`Intent: ${result['intent']['displayName']}`);
-        })
-        .catch(err => {
-            console.error('ERROR:', err);
-        });
+
+    readFile(filename).then(inputAudio => {
+        const request = {
+            session, inputAudio,
+            queryInput: {
+                audioConfig:
+                    {audioEncoding: 'AUDIO_ENCODING_LINEAR_16', sampleRateHertz: 16000, languageCode: 'es-ES'}
+            }
+        };
+        return sessionClient.detectIntent(request);
+    }).then(
+        responses => console.log(responses[0].queryResult)
+    ).catch(err => console.error('ERROR:', err));
 }
